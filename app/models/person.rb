22d18cf0926f5a35739a958d6d36321b0931c524
Katie Le <lethikatie@gmail.com>
2020-12-23 04:58:19 +0000

    Automatically backed up by Learn

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+<<<<<<< HEAD
   accepts_nested_attributes_for :addresses
+=======
+ accepts_nested_attributes_for :addresses
+>>>>>>> 32643ea3cd6059280c075bd2a77c921f6cfb239a
 end
